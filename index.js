fd53f938c20f6903acf7ca768e2ba76c9833800e
Tanya Agarwal <41121573+tngrwl@users.noreply.github.com>
2024-05-16 01:32:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require("express");
 const CrudRouter = require("./Routes/crudRoutes");
 const app = express();
 const PORT = 3001;
+const cors = require('cors');
+var bodyParser = require('body-parser')
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 
 app.use("/api/",CrudRouter)
 
